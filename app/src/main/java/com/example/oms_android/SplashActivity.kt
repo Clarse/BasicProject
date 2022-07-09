317c72b31cba36fa04358473654bcbb5358d2830
Clarse <1371309485@qq.com>
2022-07-09 16:55:50 +0800

    1

---
+++ app/src/main/java/com/example/oms_android/SplashActivity.kt
@@ -0,0 +1,7 @@
+package com.example.oms_android
+
+/**
+* @author: Clarse
+* @date: 2022/7/9
+*/class SplashActivity {
+}
---
+++ app/src/main/java/com/example/oms_android/utilities/ViewClickDelay.kt
@@ -0,0 +1,28 @@
+package com.example.utils
+
+import android.view.View
+import com.example.utils.ViewClickDelay.SPACE_TIME
+import com.example.utils.ViewClickDelay.hash
+import com.example.utils.ViewClickDelay.lastClickTime
+
+object ViewClickDelay {
+    var hash: Int = 0
+    var lastClickTime: Long = 0
+    var SPACE_TIME: Long = 1000
+}
+
+infix fun View.clicks(clickAction: () -> Unit) {
+    this.setOnClickListener {
+        if (this.hashCode() != hash) {
+            hash = this.hashCode()
+            lastClickTime = System.currentTimeMillis()
+            clickAction()
+        } else {
+            val currentTime = System.currentTimeMillis()
+            if (currentTime - lastClickTime > SPACE_TIME) {
+                lastClickTime = System.currentTimeMillis()
+                clickAction()
+            }
+        }
+    }
+}
