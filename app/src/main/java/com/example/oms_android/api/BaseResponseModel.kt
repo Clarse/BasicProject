1fc50f422329a65a60531d640d271453ef4d6c03
Clarse <1371309485@qq.com>
2022-08-30 17:23:51 +0800

    5

--- app/src/main/java/com/example/oms_android/api/BaseResponseModel.kt
+++ app/src/main/java/com/example/oms_android/api/BaseResponseModel.kt
@@ -4,6 +4,18 @@ package com.example.oms_android.api
  * @author: Clarse
  * @date: 2022/7/9
  */
-data class BaseResponseModel(val code: Int = 0, val msg: String? = null, val data: Data? = null)
-
-data class Data(val token: String)
+data class BaseResponseModel<T>(
+    val code: Int = 0,
+    val msg: String? = null,
+    val id: Int? = null,
+    val data: T? = null,
+    val success: String? = null
+) {
+    fun getResult(): Result<T> {
+        return if (code == 0) {
+            Result.success(data!!)
+        } else {
+            Result.failure(Exception("$msg"))
+        }
+    }
+}
--- app/src/main/java/com/example/oms_android/base/BaseViewModel.kt
+++ app/src/main/java/com/example/oms_android/base/BaseViewModel.kt
@@ -1,7 +1,23 @@
 package com.example.oms_android.base
 
 import androidx.lifecycle.ViewModel
+import com.example.oms_android.api.BaseResponseModel
+import com.example.oms_android.api.RetrofitClient
 
-open class BaseViewModel: ViewModel() {
+open class BaseViewModel : ViewModel() {
+    val apiService = RetrofitClient.getInstance().getService()
+
+    suspend fun <T> launcher(
+        final: () -> Unit = {},
+        block: suspend () -> BaseResponseModel<T>
+    ): Result<T> {
+        return try {
+            block().getResult()
+        } catch (e: Exception) {
+            Result.failure(e)
+        } finally {
+            final()
+        }
+    }
 
 }
